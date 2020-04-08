e123265c87f789f897891343080e87aed26b1866
Simran  Singh <simran.singh@intimetec.com>
2020-04-08 15:58:54 +0530

    mychanges

--- MyFileJenkins.sh
+++ MyFileJenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "this is an example of a scrit from github to jenkins"
-echo "Bye???"
+echo "Bye??"
 NUMBER=$((FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUMBER"
 echo "this is complete"
