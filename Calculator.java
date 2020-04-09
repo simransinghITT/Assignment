9c93b5f982e59795832f896f9b04a2a8422226ee
simransinghITT <simran.singh@intimetec.com>
2020-04-09 19:15:17 +0530

    Calc.

--- Calculator.java
+++ Calculator.java
@@ -5,18 +5,18 @@ public class Calculator {
       double ans=0;
       char $op;
       switch("op") {
-         case '+': ans = $num1 + $num2;
+         case '+': ans = $num1 + "num2";
             break;
-         case '-': ans = $num1 - $num2;
+         case '-': ans = "num1" - "num2";
             break;
-         case '*': ans = $num1 * $num2;
+         case '*': ans = "num1" * "num2";
             break;
-         case '/': ans = $num1 / $num2;
+         case '/': ans = "num1" / "num2";
             break;
          default:  System.out.printf("Error! Enter correct operator");
             return;
       }
       System.out.print("\nThe result is given as follows:\n");
-      System.out.printf($num1 + " " + $op + " " + $num2 + " = " + ans);
+      System.out.printf("num1" + " " + "op" + " " + "num2" + " = " + ans);
    }
 }
