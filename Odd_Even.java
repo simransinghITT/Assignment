be23280f55b187b1a7e21e3842750ec7fed69013
simransinghITT <simran.singh@intimetec.com>
2020-04-09 20:36:20 +0530

    Calc.

--- Odd_Even.java
+++ Odd_Even.java
@@ -3,7 +3,7 @@ public class Odd_Even
 	public static void main(String[] args) 
 		{
 
-            int i = Integer.parseInt("n");
+            int i = Integer.parseInt(n);
             if(i % 2 == 0)
 
             {
