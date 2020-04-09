b8aa80b48bfdc8f5a55b200ee8ec574108aba0c5
simransinghITT <simran.singh@intimetec.com>
2020-04-09 20:20:57 +0530

    Calc.

--- Odd_Even.java
+++ Odd_Even.java
@@ -2,9 +2,6 @@ public class Odd_Even
  {
 	public static void main(String[] args) 
 		{
-	
-
-            System.out.print("Enter the number you want to check:");
 
             if(Integer.parseInt("n") % 2 == 0)
 
