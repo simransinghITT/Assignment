0b046849a2ceb2e5e854c50fa6d9d8e095534dc9
simransinghITT <simran.singh@intimetec.com>
2020-04-10 11:28:11 +0530

    Changes

--- Odd_Even.java
+++ Odd_Even.java
@@ -1,24 +1,26 @@
 public class Odd_Even 
- {
+{
 	public static void main(String[] args) 
-		{
+	{
             
-            if(Integer.parseInt(args[0]) % 2 == 0)
+            	if(Integer.parseInt(args[0]) % 2 == 0)
 
-            {
+            	{
 
-         System.out.println("The given number" + Integer.parseInt(args[0]) + "is Even ");
+         		System.out.println(Integer.parseInt(args[0]) + " is Even ");
 
-            }
+            	}
 
-            else
+            	else
 
-            {
+            	{
 
-            System.out.println("The given number "+ Integer.parseInt(args[0]) +" is Odd ");
+            		System.out.println(Integer.parseInt(args[0]) + " is Odd ");
+			
+            		System.out.println(Integer.parseInt("n") + " is Odd ");
 
-    	}
+    		}
 
         }
 
-    }
+}
