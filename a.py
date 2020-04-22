79c33c2ebb8138e304cd1171ded0aa93f9e48cf6
simransinghITT <simran.singh@intimetec.com>
2020-04-22 12:33:49 +0530

    Code

--- a.py
+++ a.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import subprocess
+import cgi
 import os
-os.system('sshpass -p "redhat" ssh s@192.168.43.85 "mkdir ff"')
 
-
+print "Content-type: text/html\n\n"
+mydata =  cgi.FieldStorage()
+docker_name = mydata.getvalue('name')
+docker_image = mydata.getvalue('image')
+#os.system('sshpass -p "redhat" ssh s@192.168.43.85 "mkdir ttt"')
+os.system("docker run -d --name docker_name image_name")
