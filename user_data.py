e262d3cf5480281f96ec19d766bb04f4898d9f29
simransinghITT <59827120+simransinghITT@users.noreply.github.com>
2020-04-25 15:26:40 +0530

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import cgi
+import sys
 
 print("content-type: text/html")
 print()
@@ -12,13 +13,16 @@ docker_image = form.getvalue('img')
 docker_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.Names}}'")
 if (docker_name in docker_in_system):
   print("Docker already exist please choose another name.")
-  exit()
+  return
 else:
+  output=subprocess.getoutput("sudo docker run --name {} {}".format(docker_name,docker_image))
+  print("Docker {} launched successfully".format(docker_name))
+  docker_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.Names}}'")
   a=docker_in_system.split()
   print("Total docker present in system are: {}".format(len(a)))
-  print()
+  
   for i in range(0,len(a)):
     print(a[i])
-    print()
-  output=subprocess.getoutput("sudo docker run --name {} {}".format(docker_name,docker_image))
-  print("Docker {} launched successfully").format(docker_name)
+    
+  
+  sys.exit()
