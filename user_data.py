843e0be6d63b4c6bfb702f5e0203656cf384a130
simransinghITT <59827120+simransinghITT@users.noreply.github.com>
2020-04-25 17:10:32 +0530

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -17,7 +17,9 @@ else:
   output=subprocess.getoutput("sudo docker run --name {} {}".format(docker_name,docker_image))
   print("<H1>Docker '{}' launched successfully</H1>".format(docker_name))
   docker_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.Names}}'")
+  docker_image_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.images}}'")
   docker_in_system_list=docker_in_system.split()
+  docker_image_in_system_list=docker_image_in_system.split()
   print("<H3>Total docker present in system are: {}</H3>".format(len(docker_in_system_list)))
   for i in range(1,len(docker_in_system_list)+1):
-    print("<H4>{}.        {}</H4>".format(i,docker_in_system_list[i-1]))
+    print("<H4>{}. {} --- {}</H4>".format(i,docker_in_system_list[i-1],docker_image_in_system[i-1]))
