db0996f97625f05f1395d674b19b2cdc18b39e59
simransinghITT <59827120+simransinghITT@users.noreply.github.com>
2020-04-27 18:59:12 +0530

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -14,7 +14,7 @@ if(docker_name in dockers_in_system):
   print("<H1>Docker already exist please choose another name.</H1>")
   exit()
 else:
-  output=subprocess.getoutput("sudo docker run --name {} {}".format(docker_name,docker_image))
+  output=subprocess.getoutput("sudo docker run -idt --name {} {}".format(docker_name,docker_image))
   print("<H1>Docker '{}' launched successfully</H1>".format(docker_name))
   docker_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.Names}}'")
   docker_image_in_system=subprocess.getoutput("sudo docker ps -a --format '{{.Image}}'")
